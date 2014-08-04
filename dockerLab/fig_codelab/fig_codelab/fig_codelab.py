eddeb6e3a077d5524419162e37e0d614cd8bc817
Ubuntu <ubuntu@ip-172-31-47-173.us-west-2.compute.internal>
2014-08-04 17:59:15 +0000

    update

--- dockerLab/fig_codelab/fig_codelab/fig_codelab.py
+++ dockerLab/fig_codelab/fig_codelab/fig_codelab.py
@@ -3,7 +3,7 @@ import multiprocessing
 bind = "127.0.0.1:8080"
 
 def app(enfiron, start_response):
-	data = "Here's some data\n"
+	data = "Here's some data! It's running correctly!\n"
 	start_response("200 OK", [
 		("Content-Type", "text/plain"),
 		("Content-Length", str(len(data)))
--- dockerLab/fig_codelab/fig_codelab/settings.py
+++ dockerLab/fig_codelab/fig_codelab/settings.py
@@ -87,4 +87,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+#STATIC_ROOT=path.join(PROJECT_ROOT, 'static')
+#STATIC_URL=path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
